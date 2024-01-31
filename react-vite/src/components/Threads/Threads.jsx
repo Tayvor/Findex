5543dc4cb82f0611894c2ade283556ee00afb9ba
Tayvor <tabeck98@gmail.com>
2024-01-31 10:14:06 -0700

    Added new thread button.

--- react-vite/src/components/Threads/Threads.jsx
+++ react-vite/src/components/Threads/Threads.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { thunkGetThreads } from "../../redux/threads";
+import './Threads.css'
 
 
 function Threads() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(thunkGetThreads())
@@ -12,12 +15,22 @@ function Threads() {
 
   const threads = Object.values(useSelector((state) => state.threads));
 
+
   return (
     <>
       <h2>Threads:</h2>
 
+      <button
+        className="newThreadBtn clickable"
+        onClick={() => navigate('/create-thread')}
+      >+</button>
+
       {threads.map((thread) =>
-        <div key={thread.id}>
+        <div
+          key={thread.id}
+          className="threadBox clickable"
+          onClick={() => navigate(`/threads/${thread.id}`)}
+        >
           <div>{thread.title}</div>
         </div>
       )}
