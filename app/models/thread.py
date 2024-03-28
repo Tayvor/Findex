a8e13082701ffdf0d2e609693d94e1b6e3c49607
Tayvor <tabeck98@gmail.com>
2024-03-28 14:49:01 -0600

    altered migrations

--- app/models/thread.py
+++ app/models/thread.py
@@ -10,8 +10,8 @@ class Thread(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(50), nullable=False)
   description = db.Column(db.Text, nullable=False)
-  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-  community_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('communities.id')), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+  community_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('communities.id')))
   created_at = db.Column(db.String, nullable=False)
 
   user = db.relationship('User', back_populates='threads')
